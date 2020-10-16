b6a4f0f40820fbc4165c37af4cd40c8ac0858bb4
bulma-it <bulma_it@mail.ru>
2020-10-16 17:45:11 +0600

    powel nahui2

--- js/main.js
+++ js/main.js
@@ -27,5 +27,36 @@ open_nav.addEventListener('click', (e)=> {
             nav_text[s].style.display = 'none'
         }
     }
+})
 
+let message_button = document.querySelector('.message-header')
+let logist_button = document.querySelector('.logist-header')
+let tovar_button = document.querySelector('.tovar-header')
+let sklad_button = document.querySelector('.sklad-header')
+let drop_menu = document.querySelectorAll('.drop-menu')
+
+let adm_button = document.querySelector('.admin-button > div')
+
+document.addEventListener('click', function(e){
+    if (e.target.classList.contains('message-header') === adm_button.classList.contains('message-header')) {
+        drop_menu[0].style.display = 'block'
+        console.log(e.target)
+    } else {
+        drop_menu[0].removeAttribute('style')
+    }
+    if (e.target === logist_button) {
+        drop_menu[1].style.display = 'block'
+    } else {
+        drop_menu[1].removeAttribute('style')
+    }
+    if (e.target === tovar_button) {
+        drop_menu[2].style.display = 'block'
+    } else {
+        drop_menu[2].removeAttribute('style')
+    }
+    if (e.target === sklad_button) {
+        drop_menu[3].style.display = 'block'
+    } else {
+        drop_menu[3].removeAttribute('style')
+    }
 })
