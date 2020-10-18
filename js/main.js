2c671d1aedc03058873c8e91e88bb7fd806d2fe0
bulma-it <bulma_it@mail.ru>
2020-10-19 03:13:08 +0600

    v pizdu

--- js/main.js
+++ js/main.js
@@ -1,62 +1,66 @@
-let nav = document.querySelectorAll('.profile-navigation > a')
-let nav_text = document.querySelectorAll('.nav-text')
-let open_nav = document.querySelector('.open-nav')
-let sidebar = document.querySelector('.left-sidebar')
+if (document.querySelector('.left-sidebar')) {
+    let nav = document.querySelectorAll('.profile-navigation > a')
+    let nav_text = document.querySelectorAll('.nav-text')
+    let open_nav = document.querySelector('.open-nav')
+    let sidebar = document.querySelector('.left-sidebar')
 
-for (let s = 0; s < nav_text.length; s++) {
-    nav_text[s].style.display = 'none'
+    for (let s = 0; s < nav_text.length; s++) {
+        nav_text[s].style.display = 'none'
+    }
+
+    open_nav.addEventListener('click', (e)=> {
+        if (open_nav.classList.contains('check') == false) {
+            open_nav.classList.add('check')
+            sidebar.classList.add('nav-width')
+
+        } else if (open_nav.classList.contains('check')){ 
+            open_nav.removeAttribute('class')
+            open_nav.setAttribute('class', 'open-nav')
+            sidebar.classList.remove('nav-width')
+        }
+
+        if (open_nav.classList.contains('check')) {
+            for (let h = 0; h < nav_text.length; h++) {
+                nav_text[h].style.display = 'block'
+            }
+        } else {
+            for (let s = 0; s < nav_text.length; s++) {
+                nav_text[s].style.display = 'none'
+            }
+        }
+    })
 }
 
-open_nav.addEventListener('click', (e)=> {
-    if (open_nav.classList.contains('check') == false) {
-        open_nav.classList.add('check')
-        sidebar.classList.add('nav-width')
+if (document.querySelector('.admin-button')) {
+    let message_button = document.querySelector('.message-header')
+    let logist_button = document.querySelector('.logist-header')
+    let tovar_button = document.querySelector('.tovar-header')
+    let sklad_button = document.querySelector('.sklad-header')
+    let drop_menu = document.querySelectorAll('.drop-menu')
 
-    } else if (open_nav.classList.contains('check')){ 
-        open_nav.removeAttribute('class')
-        open_nav.setAttribute('class', 'open-nav')
-        sidebar.classList.remove('nav-width')
-    }
+    let adm_button = document.querySelector('.admin-button > div')
 
-    if (open_nav.classList.contains('check')) {
-        for (let h = 0; h < nav_text.length; h++) {
-            nav_text[h].style.display = 'block'
+    document.addEventListener('click', function(e){
+        if (e.target.classList.contains('message-header') === adm_button.classList.contains('message-header')) {
+            drop_menu[0].style.display = 'block'
+            console.log(e.target)
+        } else {
+            drop_menu[0].removeAttribute('style')
         }
-    } else {
-        for (let s = 0; s < nav_text.length; s++) {
-            nav_text[s].style.display = 'none'
+        if (e.target === logist_button) {
+            drop_menu[1].style.display = 'block'
+        } else {
+            drop_menu[1].removeAttribute('style')
         }
-    }
-})
-
-let message_button = document.querySelector('.message-header')
-let logist_button = document.querySelector('.logist-header')
-let tovar_button = document.querySelector('.tovar-header')
-let sklad_button = document.querySelector('.sklad-header')
-let drop_menu = document.querySelectorAll('.drop-menu')
-
-let adm_button = document.querySelector('.admin-button > div')
-
-document.addEventListener('click', function(e){
-    if (e.target.classList.contains('message-header') === adm_button.classList.contains('message-header')) {
-        drop_menu[0].style.display = 'block'
-        console.log(e.target)
-    } else {
-        drop_menu[0].removeAttribute('style')
-    }
-    if (e.target === logist_button) {
-        drop_menu[1].style.display = 'block'
-    } else {
-        drop_menu[1].removeAttribute('style')
-    }
-    if (e.target === tovar_button) {
-        drop_menu[2].style.display = 'block'
-    } else {
-        drop_menu[2].removeAttribute('style')
-    }
-    if (e.target === sklad_button) {
-        drop_menu[3].style.display = 'block'
-    } else {
-        drop_menu[3].removeAttribute('style')
-    }
-})
+        if (e.target === tovar_button) {
+            drop_menu[2].style.display = 'block'
+        } else {
+            drop_menu[2].removeAttribute('style')
+        }
+        if (e.target === sklad_button) {
+            drop_menu[3].style.display = 'block'
+        } else {
+            drop_menu[3].removeAttribute('style')
+        }
+    })
+}
